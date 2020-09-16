adce09d2443cc4c9f1a990cf5efdabda96f7765d
Javi Mollá <javier.molla.moreno@gmail.com>
2020-09-16 18:11:21 +0200

    Check single permission with Dexter

--- app/src/main/java/com/example/desugaring/MainActivity.kt
+++ app/src/main/java/com/example/desugaring/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.desugaring
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkDevicePermissions()
+    }
+
+    private fun checkDevicePermissions() {
+        Dexter.withContext(this@MainActivity)
+            .withPermission(Manifest.permission.CAMERA)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest,
+                    token: PermissionToken
+                ) {
+                }
+            }).check()
     }
 }
